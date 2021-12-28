1659c1481aa80e53269bc424079343783c68c529
Juraj Nyíri <jnyiri@sk.ibm.com>
2021-12-28 16:56:47 +0100

    Add: Clock

--- custom_components/dwains_dashboard/cards/dwains-notification-card/dwains-notification-card.js
+++ custom_components/dwains_dashboard/cards/dwains-notification-card/dwains-notification-card.js
@@ -1,49 +1,66 @@
-!function(t){var i={};function e(n){if(i[n])return i[n].exports;var o=i[n]={i:n,l:!1,exports:{}};return t[n].call(o.exports,o,o.exports,e),o.l=!0,o.exports}e.m=t,e.c=i,e.d=function(t,i,n){e.o(t,i)||Object.defineProperty(t,i,{enumerable:!0,get:n})},e.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},e.t=function(t,i){if(1&i&&(t=e(t)),8&i)return t;if(4&i&&"object"==typeof t&&t&&t.__esModule)return t;var n=Object.create(null);if(e.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:t}),2&i&&"string"!=typeof t)for(var o in t)e.d(n,o,function(i){return t[i]}.bind(null,o));return n},e.n=function(t){var i=t&&t.__esModule?function(){return t.default}:function(){return t};return e.d(i,"a",i),i},e.o=function(t,i){return Object.prototype.hasOwnProperty.call(t,i)},e.p="",e(e.s=0)}([function(t,i,e){const n=[customElements.whenDefined("hui-masonry-view"),customElements.whenDefined("hc-lovelace")];Promise.race(n).then(()=>{const t=customElements.get("hui-masonry-view")?Object.getPrototypeOf(customElements.get("hui-masonry-view")):Object.getPrototypeOf(customElements.get("hc-lovelace")),i=t.prototype.html,n=t.prototype.css;class o extends t{constructor(){super(),this.notifications=null}static get properties(){return{hass:{},config:{}}}setConfig(t){this.config=t}async _subcribeNotifications(){this._unsub||(this._unsub=await this.hass.connection.subscribeEvents(()=>this._notificationsUpdated(),"dwains_dashboard_notifications_updated"))}disconnectedCallback(){super.disconnectedCallback(),this._unsub&&(this._unsub(),this._unsub=void 0)}connectedCallback(){super.connectedCallback(),this._unsub||(this._subcribeNotifications(),this._notificationsUpdated())}async _notificationsUpdated(){this.notifications=await this.hass.callWS({type:"dwains_dashboard_notification/get"})}_handleDismiss(t){var i=t.target.notificationId;this.hass.callService("dwains_dashboard","notification_dismiss",{notification_id:i})}_renderNotification(t){return i`
-        <div class="notification">
-          ${t.message}
-          ${!1===this.config.show_dismiss?i``:i`<ha-icon icon='mdi:close' .notificationId='${t.notification_id}' @click=${this._handleDismiss}></ha-icon>`}
-        </div>
-      `}_evalTemplate(t){return new Function("states","user","hass","variables","'use strict'; "+t).call(this,this.hass.states,this.hass.user,this.hass,this.config.variables)}_handleTap(){let t;this.config.navigation_path&&(history.pushState(null,"",this.config.navigation_path),t=new Event("location-changed",{composed:!0}),t.detail={replace:!1},window.dispatchEvent(t))}render(){var t;if(this.config.title){const i=this.config.title,e=i.trim();t="[[["===e.substring(0,3)&&"]]]"===e.slice(-3)?this._evalTemplate(e.slice(3,-3)):i}if(null==this.notifications||0===this.notifications.length)var e=i`
-          ${this.hass.localize("ui.notification_drawer.empty")}
-        `;else{var n;if(this.config.max_notifications&&this.notifications.length>this.config.max_notifications){var o=this.notifications.slice(0,this.config.max_notifications);n=i`
-                      <div class="more-link" @click="${this._handleTap}">
-                        and ${this.notifications.length-this.config.max_notifications} more<ha-icon icon='mdi:chevron-right'></ha-icon>
-                      </div>
-                    `}else o=this.notifications;e=i`
-          <div id="notifications">
-            ${o.map(t=>this._renderNotification(t))}
-            ${n}
-          </div>
-        `}return i`
-        ${this.config.style?i`
-          <style>
-            ${this.config.style}
-          </style>
-        `:i``}
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
 
-        <ha-card style="background: transparent;">
-          <div id="title">
-            ${t}
-          </div>
-          ${e}
-        </ha-card>
-      `}static get styles(){return n`
-        ha-card {
-          box-shadow: none;
-          color: var(--text-primary-color);
-        }
-        #title {
-          padding-bottom: 3px;
-        }
-        .notification {
-          filter: brightness(90%);
-        }
-        .notification ha-icon {
-          height: 15px;
-          width: 15px;
-        }
-        .more-link {
-          font-weight: bold;
-          font-size: 85%;
-        }
-      `}getCardSize(){return 2}}if(!customElements.get("dwains-notification-card")){customElements.define("dwains-notification-card",o);const t=e(1);console.info(`%c DWAINS-NOTIFICATION-CARD  \n%c       Version ${t.version}       `,"color: #2fbae5; font-weight: bold; background: black","color: white; font-weight: bold; background: dimgray")}})},function(t){t.exports=JSON.parse('{"name":"dwains-notification-card","private":true,"version":"0.0.5","description":"dwains-notification-card","scripts":{"build":"webpack","watch":"webpack --watch --mode=development","update-card-tools":"npm uninstall card-tools && npm install thomasloven/lovelace-card-tools"},"keywords":[],"author":"Dwain Scheeren","license":"MIT","devDependencies":{"webpack":"^4.42.1","webpack-cli":"^3.3.11"},"dependencies":{"card-tools":"github:thomasloven/lovelace-card-tools"}}')}]);
+/***/ "./package.json":
+/*!**********************!*\
+  !*** ./package.json ***!
+  \**********************/
+/***/ ((module) => {
+
+"use strict";
+eval("module.exports = JSON.parse('{\"name\":\"dwains-notification-card\",\"private\":true,\"version\":\"0.0.5\",\"description\":\"dwains-notification-card\",\"scripts\":{\"build\":\"webpack\",\"watch\":\"webpack --watch --mode=development\",\"update-card-tools\":\"npm uninstall card-tools && npm install thomasloven/lovelace-card-tools\"},\"keywords\":[],\"author\":\"Dwain Scheeren\",\"license\":\"MIT\",\"devDependencies\":{\"webpack\":\"^4.42.1\",\"webpack-cli\":\"^3.3.11\"},\"dependencies\":{\"card-tools\":\"github:thomasloven/lovelace-card-tools\"}}');\n\n//# sourceURL=webpack://dwains-notification-card/./package.json?");
+
+/***/ }),
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("const bases = [\r\n  customElements.whenDefined(\"hui-masonry-view\"),\r\n  customElements.whenDefined(\"hc-lovelace\"),\r\n];\r\nPromise.race(bases).then(() => {\r\n  const LitElement = customElements.get(\"hui-masonry-view\")\r\n    ? Object.getPrototypeOf(customElements.get(\"hui-masonry-view\"))\r\n    : Object.getPrototypeOf(customElements.get(\"hc-lovelace\"));\r\n\r\n  const html = LitElement.prototype.html;\r\n\r\n  const css = LitElement.prototype.css;\r\n\r\n  class DwainsNotificationCard extends LitElement {\r\n    constructor() {\r\n      super();\r\n\r\n      this.notifications = null;\r\n    }\r\n\r\n    static get properties() {\r\n      return {\r\n        hass: {},\r\n        config: {},\r\n      };\r\n    }\r\n\r\n    setConfig(config) {\r\n      this.config = config;\r\n    }\r\n\r\n    async _subcribeNotifications() {\r\n      if (!this._unsub) {\r\n        this._unsub = await this.hass.connection.subscribeEvents(\r\n          () => this._notificationsUpdated(),\r\n          \"dwains_dashboard_notifications_updated\"\r\n        );\r\n      }\r\n    }\r\n\r\n    disconnectedCallback() {\r\n      super.disconnectedCallback();\r\n      if (this._unsub) {\r\n        this._unsub();\r\n        this._unsub = undefined;\r\n      }\r\n    }\r\n\r\n    connectedCallback() {\r\n      super.connectedCallback();\r\n      if (!this._unsub) {\r\n        this._subcribeNotifications();\r\n        this._notificationsUpdated(); //Load notifications\r\n      }\r\n    }\r\n\r\n    async _notificationsUpdated() {\r\n      this.notifications = await this.hass.callWS({\r\n        type: \"dwains_dashboard_notification/get\",\r\n      });\r\n    }\r\n\r\n    _handleDismiss(event) {\r\n      var id = event.target.notificationId;\r\n      this.hass.callService(\"dwains_dashboard\", \"notification_dismiss\", {\r\n        notification_id: id,\r\n      });\r\n    }\r\n\r\n    _renderNotification(n) {\r\n      return html`\r\n        <div class=\"notification\">\r\n          ${n.message}\r\n          ${this.config.show_dismiss === false\r\n            ? html``\r\n            : html`<ha-icon\r\n                icon=\"mdi:close\"\r\n                .notificationId=\"${n.notification_id}\"\r\n                @click=${this._handleDismiss}\r\n              ></ha-icon>`}\r\n        </div>\r\n      `;\r\n    }\r\n\r\n    _evalTemplate(func) {\r\n      /* eslint no-new-func: 0 */\r\n      return new Function(\r\n        \"states\",\r\n        \"user\",\r\n        \"hass\",\r\n        \"variables\",\r\n        `'use strict'; ${func}`\r\n      ).call(\r\n        this,\r\n        this.hass.states,\r\n        this.hass.user,\r\n        this.hass,\r\n        this.config.variables\r\n      );\r\n    }\r\n\r\n    _handleTap() {\r\n      let e;\r\n\r\n      if (!this.config.navigation_path) return;\r\n      history.pushState(null, \"\", this.config.navigation_path);\r\n      e = new Event(\"location-changed\", { composed: true });\r\n      e.detail = { replace: false };\r\n      window.dispatchEvent(e);\r\n\r\n      // if(this.config.tap_action){\r\n      //   if(this.config.tap_action.action == 'navigate'){\r\n      //     window.location.href = this.config.tap_action.navigation_path;\r\n      //   }\r\n      // }\r\n    }\r\n\r\n    render() {\r\n      var title;\r\n\r\n      if (this.config.title) {\r\n        const value = this.config.title;\r\n        const trimmed = value.trim();\r\n\r\n        if (trimmed.substring(0, 3) === \"[[[\" && trimmed.slice(-3) === \"]]]\") {\r\n          title = this._evalTemplate(trimmed.slice(3, -3));\r\n        } else {\r\n          title = value;\r\n        }\r\n      }\r\n\r\n      if (this.notifications == null || this.notifications.length === 0) {\r\n        var notifications = html`\r\n          ${this.hass.localize(\"ui.notification_drawer.empty\")}\r\n        `;\r\n      } else {\r\n        var moreText;\r\n\r\n        if (\r\n          this.config.max_notifications &&\r\n          this.notifications.length > this.config.max_notifications\r\n        ) {\r\n          var items = this.notifications.slice(\r\n            0,\r\n            this.config.max_notifications\r\n          );\r\n          moreText = html`\r\n            <div class=\"more-link\" @click=\"${this._handleTap}\">\r\n              and ${this.notifications.length - this.config.max_notifications}\r\n              more<ha-icon icon=\"mdi:chevron-right\"></ha-icon>\r\n            </div>\r\n          `;\r\n        } else {\r\n          var items = this.notifications;\r\n        }\r\n\r\n        var notifications = html`\r\n          <div id=\"notifications\">\r\n            ${items.map((i) => this._renderNotification(i))} ${moreText}\r\n          </div>\r\n        `;\r\n      }\r\n      var d = new Date(),\r\n        h = (d.getHours() < 10 ? \"0\" : \"\") + d.getHours(),\r\n        m = (d.getMinutes() < 10 ? \"0\" : \"\") + d.getMinutes();\r\n\r\n      return html`\r\n        ${this.config.style\r\n          ? html`\r\n              <style>\r\n                ${this.config.style}\r\n              </style>\r\n            `\r\n          : html``}\r\n\r\n        <ha-card style=\"background: transparent;\">\r\n          <div id=\"title\">${h + \":\" + m}</div>\r\n          ${notifications}\r\n        </ha-card>\r\n      `;\r\n    }\r\n\r\n    static get styles() {\r\n      return css`\r\n        ha-card {\r\n          box-shadow: none;\r\n          color: var(--text-primary-color);\r\n        }\r\n        #title {\r\n          padding-bottom: 3px;\r\n        }\r\n        .notification {\r\n          filter: brightness(90%);\r\n        }\r\n        .notification ha-icon {\r\n          height: 15px;\r\n          width: 15px;\r\n        }\r\n        .more-link {\r\n          font-weight: bold;\r\n          font-size: 85%;\r\n        }\r\n      `;\r\n    }\r\n\r\n    getCardSize() {\r\n      return 2;\r\n    }\r\n  }\r\n\r\n  if (!customElements.get(\"dwains-notification-card\")) {\r\n    customElements.define(\"dwains-notification-card\", DwainsNotificationCard);\r\n    const pjson = __webpack_require__(/*! ../package.json */ \"./package.json\");\r\n    console.info(\r\n      `%c DWAINS-NOTIFICATION-CARD  \\n%c       Version ${pjson.version}       `,\r\n      \"color: #2fbae5; font-weight: bold; background: black\",\r\n      \"color: white; font-weight: bold; background: dimgray\"\r\n    );\r\n  }\r\n});\r\n\n\n//# sourceURL=webpack://dwains-notification-card/./src/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/main.js");
+/******/ 	
+/******/ })()
+;
--- custom_components/dwains_dashboard/cards/dwains-notification-card/src/main.js
+++ custom_components/dwains_dashboard/cards/dwains-notification-card/src/main.js
@@ -1,14 +1,15 @@
-const bases = [customElements.whenDefined('hui-masonry-view'), customElements.whenDefined('hc-lovelace')];
+const bases = [
+  customElements.whenDefined("hui-masonry-view"),
+  customElements.whenDefined("hc-lovelace"),
+];
 Promise.race(bases).then(() => {
-
-  const LitElement = customElements.get('hui-masonry-view')
-    ? Object.getPrototypeOf(customElements.get('hui-masonry-view'))
-    : Object.getPrototypeOf(customElements.get('hc-lovelace'));
+  const LitElement = customElements.get("hui-masonry-view")
+    ? Object.getPrototypeOf(customElements.get("hui-masonry-view"))
+    : Object.getPrototypeOf(customElements.get("hc-lovelace"));
 
   const html = LitElement.prototype.html;
 
   const css = LitElement.prototype.css;
-
 
   class DwainsNotificationCard extends LitElement {
     constructor() {
@@ -28,9 +29,12 @@ Promise.race(bases).then(() => {
       this.config = config;
     }
 
-    async _subcribeNotifications(){
+    async _subcribeNotifications() {
       if (!this._unsub) {
-        this._unsub = await this.hass.connection.subscribeEvents(() => this._notificationsUpdated(), "dwains_dashboard_notifications_updated");
+        this._unsub = await this.hass.connection.subscribeEvents(
+          () => this._notificationsUpdated(),
+          "dwains_dashboard_notifications_updated"
+        );
       }
     }
 
@@ -42,7 +46,7 @@ Promise.race(bases).then(() => {
       }
     }
 
-    connectedCallback(){
+    connectedCallback() {
       super.connectedCallback();
       if (!this._unsub) {
         this._subcribeNotifications();
@@ -50,44 +54,59 @@ Promise.race(bases).then(() => {
       }
     }
 
-    async _notificationsUpdated(){
+    async _notificationsUpdated() {
       this.notifications = await this.hass.callWS({
-        type: 'dwains_dashboard_notification/get'
+        type: "dwains_dashboard_notification/get",
       });
     }
 
     _handleDismiss(event) {
       var id = event.target.notificationId;
       this.hass.callService("dwains_dashboard", "notification_dismiss", {
-        notification_id: id
-      });   
+        notification_id: id,
+      });
     }
 
     _renderNotification(n) {
       return html`
         <div class="notification">
           ${n.message}
-          ${this.config.show_dismiss === false ? html ``: 
-          html`<ha-icon icon='mdi:close' .notificationId='${n.notification_id}' @click=${this._handleDismiss}></ha-icon>`}
+          ${this.config.show_dismiss === false
+            ? html``
+            : html`<ha-icon
+                icon="mdi:close"
+                .notificationId="${n.notification_id}"
+                @click=${this._handleDismiss}
+              ></ha-icon>`}
         </div>
       `;
     }
 
-    _evalTemplate(func){
+    _evalTemplate(func) {
       /* eslint no-new-func: 0 */
-      return new Function('states', 'user', 'hass', 'variables',
-        `'use strict'; ${func}`)
-        .call(this, this.hass.states, this.hass.user, this.hass, this.config.variables);
+      return new Function(
+        "states",
+        "user",
+        "hass",
+        "variables",
+        `'use strict'; ${func}`
+      ).call(
+        this,
+        this.hass.states,
+        this.hass.user,
+        this.hass,
+        this.config.variables
+      );
     }
 
-    _handleTap(){
+    _handleTap() {
       let e;
 
       if (!this.config.navigation_path) return;
-        history.pushState(null, '', this.config.navigation_path);
-        e = new Event('location-changed', { composed: true });
-        e.detail = { replace: false };
-        window.dispatchEvent(e);
+      history.pushState(null, "", this.config.navigation_path);
+      e = new Event("location-changed", { composed: true });
+      e.detail = { replace: false };
+      window.dispatchEvent(e);
 
       // if(this.config.tap_action){
       //   if(this.config.tap_action.action == 'navigate'){
@@ -99,57 +118,63 @@ Promise.race(bases).then(() => {
     render() {
       var title;
 
-      if(this.config.title){
+      if (this.config.title) {
         const value = this.config.title;
         const trimmed = value.trim();
 
-        if (
-          trimmed.substring(0, 3) === '[[['
-          && trimmed.slice(-3) === ']]]'
-        ) {
+        if (trimmed.substring(0, 3) === "[[[" && trimmed.slice(-3) === "]]]") {
           title = this._evalTemplate(trimmed.slice(3, -3));
         } else {
           title = value;
         }
       }
 
-      if(this.notifications == null || this.notifications.length === 0){
+      if (this.notifications == null || this.notifications.length === 0) {
         var notifications = html`
-          ${this.hass.localize('ui.notification_drawer.empty')}
+          ${this.hass.localize("ui.notification_drawer.empty")}
         `;
       } else {
         var moreText;
 
-        if(this.config.max_notifications && this.notifications.length > this.config.max_notifications){
-          var items = this.notifications.slice(0, this.config.max_notifications);
+        if (
+          this.config.max_notifications &&
+          this.notifications.length > this.config.max_notifications
+        ) {
+          var items = this.notifications.slice(
+            0,
+            this.config.max_notifications
+          );
           moreText = html`
-                      <div class="more-link" @click="${this._handleTap}">
-                        and ${this.notifications.length-this.config.max_notifications} more<ha-icon icon='mdi:chevron-right'></ha-icon>
-                      </div>
-                    `;
+            <div class="more-link" @click="${this._handleTap}">
+              and ${this.notifications.length - this.config.max_notifications}
+              more<ha-icon icon="mdi:chevron-right"></ha-icon>
+            </div>
+          `;
         } else {
           var items = this.notifications;
         }
 
         var notifications = html`
           <div id="notifications">
-            ${items.map((i) => this._renderNotification(i))}
-            ${moreText}
+            ${items.map((i) => this._renderNotification(i))} ${moreText}
           </div>
         `;
-
       }
+      var d = new Date(),
+        h = (d.getHours() < 10 ? "0" : "") + d.getHours(),
+        m = (d.getMinutes() < 10 ? "0" : "") + d.getMinutes();
+
       return html`
-        ${this.config.style ? html`
-          <style>
-            ${this.config.style}
-          </style>
-        ` : html``}
+        ${this.config.style
+          ? html`
+              <style>
+                ${this.config.style}
+              </style>
+            `
+          : html``}
 
         <ha-card style="background: transparent;">
-          <div id="title">
-            ${title}
-          </div>
+          <div id="title">${h + ":" + m}</div>
           ${notifications}
         </ha-card>
       `;
@@ -175,21 +200,21 @@ Promise.race(bases).then(() => {
           font-weight: bold;
           font-size: 85%;
         }
-      `
+      `;
     }
-    
+
     getCardSize() {
       return 2;
     }
   }
 
-  if(!customElements.get("dwains-notification-card")) {
+  if (!customElements.get("dwains-notification-card")) {
     customElements.define("dwains-notification-card", DwainsNotificationCard);
-    const pjson = require('../package.json');
+    const pjson = require("../package.json");
     console.info(
       `%c DWAINS-NOTIFICATION-CARD  \n%c       Version ${pjson.version}       `,
-      'color: #2fbae5; font-weight: bold; background: black',
-      'color: white; font-weight: bold; background: dimgray',
+      "color: #2fbae5; font-weight: bold; background: black",
+      "color: white; font-weight: bold; background: dimgray"
     );
   }
 });
